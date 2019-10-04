41fcb6939ee2e5ea077de174d16d4639f7f261fe
daniel-fajan <daniel.anselmi@bandtec.com.br>
2019-10-03 21:01:27 -0300

    arrumei o cadastro

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -1,23 +1,33 @@
+
+
 function funcao_cadastrar()
 {
-    if (nome_usuario.value == "")
+    if (nome_usuario.value == "" || nome_usuario.value.length < 3)
     {
-        alert(`Insira um nome de usuário válido para continuar`)
+        alert (`Insira um nome de usuário válido para continuar`);
     }
-    if (email.value == "")
+    if (email.value == "" || (email.value.indexOf('@') == -1) || (email.value.endsWith('.com.br') == -1) || (email.value.indexOf('.com') == -1))
     {
-        alert(`Insira um e-mail válido para continuar`)
+        alert (`Insira um e-mail válido para continuar`);
     }
-    if (senha.value == "")
+    //if (email.value.indexOf('.com') == -1)
+   // {
+       // alert(`Insira um e-mail válido para continuar`);
+ //   }
+    if (senha.value == "" || senha.value.length < 5)
     {
-        alert(`Insira uma senha válida para continuar`)
+        alert (`Insira uma senha válida para continuar`);
     }
-    if (confirmacao_senha == "" || confirmacao_senha.value != senha.value)
+    if (confirmacao_senha.value == "" || confirmacao_senha.value != senha.value)
     {
-        alert ("As senhas não coincidem")
+        alert ("As senhas não coincidem");
     }
-    if (nome_usuario.value != "" && email.value != "" && senha.value != "" && confirmacao_senha.value != "" && confirmacao_senha.value == senha.value)
+    if ((nome_usuario.value.length >= 3) && (nome_usuario.value != "") && (email.value != "") && (senha.value != "") && (confirmacao_senha.value != "") && (confirmacao_senha.value == senha.value) && (email.value.endsWith('.com') || email.value.endsWith('.com.br')) && (email.value.indexOf('@') >= 0) && (email.value.length >= 9))
     {
-        alert(`Parabéns! Cadastro concluído com sucesso!`)
+        alert (`Parabéns! Cadastro concluído com sucesso!`);
+    }
+    else
+    {
+        alert ("Informações incorretas! Revise os campos")
     }
 }
