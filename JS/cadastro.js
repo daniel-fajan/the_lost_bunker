9a9c03df56978afc0111015aed6a01e3397da7ba
daniel-fajan <daniel.anselmi@bandtec.com.br>
2019-10-07 20:06:53 -0300

    alterei a tela de cadastro e arrumei as imagens

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -10,10 +10,6 @@ function funcao_cadastrar()
     {
         alert (`Insira um e-mail válido para continuar`);
     }
-    //if (email.value.indexOf('.com') == -1)
-   // {
-       // alert(`Insira um e-mail válido para continuar`);
- //   }
     if (senha.value == "" || senha.value.length < 5)
     {
         alert (`Insira uma senha válida para continuar`);
@@ -22,7 +18,7 @@ function funcao_cadastrar()
     {
         alert ("As senhas não coincidem");
     }
-    if ((nome_usuario.value.length >= 3) && (nome_usuario.value != "") && (email.value != "") && (senha.value != "") && (confirmacao_senha.value != "") && (confirmacao_senha.value == senha.value) && (email.value.endsWith('.com') || email.value.endsWith('.com.br')) && (email.value.indexOf('@') >= 0) && (email.value.length >= 9))
+    if ((nome_usuario.value.length >= 3) && (nome_usuario.value != "") && (email.value != "") && (senha.value.length >= 5) && (senha.value != "") && (confirmacao_senha.value != "") && (confirmacao_senha.value == senha.value) && (email.value.endsWith('.com') || email.value.endsWith('.com.br')) && (email.value.indexOf('@') >= 0) && (email.value.length >= 9))
     {
         alert (`Parabéns! Cadastro concluído com sucesso!`);
     }
