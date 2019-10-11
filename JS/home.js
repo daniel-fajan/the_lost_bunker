683efc3b532d28ea650bcee3c257b145a560b8b6
daniel-fajan <daniel.anselmi@bandtec.com.br>
2019-10-10 21:02:01 -0300

    home

---
+++ JS/home.js
@@ -0,0 +1,58 @@
+
+var slideIndex = 1;
+
+mostrar_slide(slideIndex);
+
+
+function trocar_slide(n)
+{
+  mostrar_slide(slideIndex += n);
+}
+
+
+function currentSlide(n)
+{
+  mostrar_slide(slideIndex = n);
+}
+
+function showSlides(n)
+{
+  var i;
+  var slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length)
+  {
+    slideIndex = 1
+  }
+
+  if (n < 1)
+  {
+    slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++)
+  {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+
+
+/*
+var slideIndex = 0;
+showSlides();
+
+function showSlide() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}s
